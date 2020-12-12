241fa4655e044dbe50263c726d94b76029d061c3
SzymonWelter <szymon.welter98@gmail.com>
2020-12-12 20:53:52 +0100

    load balancing manager created

---
+++ Alley.Context/LoadBalancing/LoadBalancingManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Alley.Utils.Models;
+
+namespace Alley.Context.LoadBalancing
+{
+    public class LoadBalancingManager : IConnectionTargetProvider
+    {
+        private readonly ILoadBalancingStrategy _strategy;
+        private readonly IReadonlyInstanceContext _instanceContext;
+
+        public LoadBalancingManager(
+            IReadonlyInstanceContext instanceContext, 
+            ILoadBalancingStrategy strategy)
+        {
+            _instanceContext = instanceContext;
+            _strategy = strategy;
+        }
+        
+        public Result<Uri> GetTarget(string serviceName)
+        {
+            var instancesResult = _instanceContext.GetInstances(serviceName);
+            if (instancesResult.IsFailure)
+            {
+                return Result<Uri>.Failure(instancesResult.Message);
+            }
+            var result = _strategy.Execute(instancesResult.Value);
+            return result;
+        }
+    }
+}
