cfbd6321120e7b0020396e0656685f1e617d54b8
SzymonWelter <szymon.welter98@gmail.com>
2020-12-14 00:45:38 +0100

    service provider

--- Alley.Utils/AlleyServiceProviderFactory.cs
+++ Alley.Utils/AlleyServiceProviderFactory.cs
@@ -1,7 +1,42 @@
-﻿namespace Alley.Utils
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Alley.Utils
 {
-    public class AlleyServiceProvider
+    public class AlleyServiceProviderFactory : IServiceProviderFactory<IServiceCollection>
     {
-        
+        private readonly IServiceCollection _serviceCollection;
+        private IServiceProvider _provider;
+
+        public AlleyServiceProviderFactory()
+        {
+            _serviceCollection = new ServiceCollection();
+        }
+
+        public IServiceCollection CreateBuilder(IServiceCollection services)
+        {
+            var serviceDescriptors = new ServiceDescriptor[services.Count];
+            services.CopyTo(serviceDescriptors, 0);
+
+            foreach (var service in serviceDescriptors)
+            {
+                if (!_serviceCollection.Contains(service))
+                {
+                    _serviceCollection.Add(service);
+                }
+            }
+
+            return _serviceCollection;
+        }
+
+        public IServiceProvider CreateServiceProvider(IServiceCollection containerBuilder)
+        {
+            CreateBuilder(containerBuilder);
+            return CreateServiceProvider();
+        }
+        public IServiceProvider CreateServiceProvider()
+        {
+            return _provider ??= _serviceCollection.BuildServiceProvider();
+        }
     }
 }
