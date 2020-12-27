16ad555db7d88fc280034255f5aa31c5660482b8
SzymonWelter <szymon.welter98@gmail.com>
2020-12-27 19:39:16 +0100

    create monitoring dtos

---
+++ Alley.Monitoring/Models/MetricsResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Alley.Monitoring.Models
+{
+    public class MetricsResult
+    {
+        public string ResultType { get; set; }
+        public MetricResult [] Result { get; set; }
+    }
+}
