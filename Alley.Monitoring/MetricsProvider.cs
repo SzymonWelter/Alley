b9e0254fa438d9c9f7ff45cc8f97edecb5a737bb
SzymonWelter <szymon.welter98@gmail.com>
2020-12-29 22:18:39 +0100

    metrics registration

---
+++ Alley.Monitoring/MetricsProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Alley.Monitoring.Models;
+
+namespace Alley.Monitoring
+{
+    public class MetricsProvider : IMetricsProvider
+    {
+        private readonly IEnumerable<IMetricFetcher> _fetchers;
+
+        public MetricsProvider(IFetchersProvider fetchersProvider)
+        {
+            _fetchers = fetchersProvider.GetFetchers();
+        }
+        public async Task<IEnumerable<MetricsSummary>> GetMetrics()
+        {
+            var tasks = _fetchers.Select(f => f.Fetch());
+            var result = await Task.WhenAll(tasks);
+            return result;
+        }
+    }
+}
