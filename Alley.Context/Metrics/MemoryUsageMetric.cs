e590d66d8fe3ee88475b5f19cd6e86428c363d06
SzymonWelter <szymon.welter98@gmail.com>
2020-12-29 22:12:01 +0100

    added memory usage metric and strategy

---
+++ Alley.Context/Metrics/MemoryUsageMetric.cs
@@ -0,0 +1,23 @@
+﻿namespace Alley.Context.Metrics
+{
+    public class MemoryUsageMetric : IInstanceMetric
+    {
+        public double Value { get; }
+        public MetricType Type { get; }
+        public MemoryUsageMetric(double value)
+        {
+            Value = value;
+            Type = MetricType.ProcessorUsage;
+        }
+
+        public int CompareTo(IInstanceMetric other)
+        {
+            return Value.CompareTo(other.Value);
+        }
+
+        public override string ToString()
+        {
+            return $"{{{Type.ToString()} : {Value}}}";
+        }
+    }
+}
